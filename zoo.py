55cacf3708c9197e55b72901200968ac6d927183
Ant1955 <abb1955@gmail.com>
2024-05-22 19:48:24 +0300

    Final release

--- zoo.py
+++ zoo.py
@@ -1,4 +1,7 @@
+import pickle
 import os
+
+
 class Animal:
     def __init__(self, name, age):
         self.name = name
@@ -10,21 +13,26 @@ class Animal:
     def eat(self):
         print(f"{self.name} кушает.")
 
+
 class Bird(Animal):
     def make_sound(self):
         print(f"{self.name} поет.")
+
 
 class Mammal(Animal):
     def make_sound(self):
         print(f"{self.name} храпит.")
 
+
 class Reptile(Animal):
     def make_sound(self):
         print(f"{self.name} шипит.")
 
+
 def animal_sound(animals):
     for animal in animals:
         animal.make_sound()
+
 
 class Zoo:
     def __init__(self):
@@ -37,28 +45,35 @@ class Zoo:
     def add_staff(self, person):
         self.staff.append(person)
 
+
 class ZooKeeper:
+
     def __init__(self, name):
         self.name = name
 
     def feed_animal(self, animal):
-        print(f"{self.name} is feeding {animal.name}.")
+        print(f"{self.name} кормит {animal.name}.")
+
 
 class Veterinarian:
     def __init__(self, name):
         self.name = name
+
     def heal_animal(self, animal):
-        print(f"{self.name} is healing {animal.name}.")
+        print(f"{self.name} лечит {animal.name}.")
 
 # Сохранение состояния зоопарка в файл
-def save_zoo(zoo, filename):
+
+
+def save_zoo(zoo_name, filename):
     with open(filename, 'wb') as file:
-        file.write(zoo)
+        pickle.dump(zoo_name, file)
+
 
 # Загрузка состояния зоопарка из файла
 def load_zoo(filename):
     with open(filename, 'rb') as file:
-        return file.read()
+        return pickle.load(file)
 
 
 zoo = Zoo()
@@ -69,20 +84,16 @@ zoo.add_animal(Reptile("Змея", 2))
 zoo.add_staff(ZooKeeper("Михаил"))
 zoo.add_staff(Veterinarian("Светлана"))
 
-if os.path.exists("zoo.txt"):
-    zoo = load_zoo("zoo.txt")
 
-else: # if not os.path.exists("zoo.txt"):
+if os.path.exists("zoo.txt") & (input("Загрузить состояние зоопарка? (y/n): ") == "y"):
+    zoo = load_zoo("zoo.txt")
+else:  # if not os.path.exists("zoo.txt"):
     save_zoo(zoo, "zoo.txt")
 
-save_zoo(zoo, "zoo.txt")
-
 animal_sound(zoo.animals)
 
 for person in zoo.staff:
-    person.feed_animal(zoo.animals[0])
-
-for person in zoo.staff:
-    person.heal_animal(zoo.animals[1])
-
-animal_sound(zoo.animals)
+    if isinstance(person, ZooKeeper):
+        person.feed_animal(zoo.animals[0])
+    else:
+        person.heal_animal(zoo.animals[1])
